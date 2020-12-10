7a10c2bb9debd0b536101d8cb70ab858cfa40b21
Chetan <56998650+techthatmatters@users.noreply.github.com>
2020-12-10 17:43:06 +0530

    Add dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
-import './card_manager.dart';
+// import './card_manager.dart';
+import 'package:dio/dio.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  getDioData() async {
+    var dio = Dio();
+    Response response =
+        await dio.get('https://jsonplaceholder.typicode.com/photos');
+    print(response.data[0]);
+    return response.data;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'ChetanApp',
       theme: ThemeData(primarySwatch: Colors.blueGrey),
       // visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -14,7 +29,46 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('ChetanApp'),
         ),
-        body: ProductManager(),
+        // body: ProductManager(),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: Text(
+                'data',
+                style: TextStyle(backgroundColor: Colors.blue),
+              ),
+            ),
+            ListView(
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              children: const <Widget>[
+                Card(
+                  child: ListTile(
+                    leading: FlutterLogo(size: 72.0),
+                    title: Text('s'),
+                    subtitle: Text(
+                        'A sufficiently long subtitle warrants three lines.'),
+                    trailing: Icon(
+                      Icons.more_vert,
+                    ),
+                    isThreeLine: true,
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: const Icon(Icons.network_check_sharp),
+                    title: const Text("Trix's airplane"),
+                    subtitle: Text('The airplane is only in Act II.'),
+                    trailing: Icon(
+                      Icons.more_vert,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
